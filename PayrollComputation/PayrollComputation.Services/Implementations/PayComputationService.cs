4c9d868a4d29f7c098b2c81dd63fa22ce7962c56
Ikechukwu <ikechukwu.njoku@decagon.dev>
2021-11-19 11:03:58 +0100

    Completed the Pay Computation service

---
+++ PayrollComputation/PayrollComputation.Services/Implementations/PayComputationService.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using PayrollComputation.Data;
+using PayrollComputation.Model;
+using PayrollComputation.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollComputation.Services.Implementations
+{
+    public class PayComputationService : IPayComputation
+    {
+        private readonly ApplicationDbContext _db;
+        private decimal contractualEarnings;
+        private decimal overtimeHours;
+        public PayComputationService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public decimal ContractualEarnings(decimal contractualHours, decimal hoursWorked, decimal hourlyRate)
+        {
+            if(hoursWorked < contractualHours) 
+            {
+                contractualEarnings = hoursWorked * hourlyRate;
+            }
+            else 
+            {
+                contractualEarnings = contractualHours * hourlyRate;
+            }
+
+            return contractualEarnings;
+        }
+
+        public async Task CreateAsync(PaymentRecord paymentRecord)
+        {
+            await _db.PaymentRecords.AddAsync(paymentRecord);
+            await _db.SaveChangesAsync();
+        }
+
+        public IEnumerable<PaymentRecord> GetAll() => _db.PaymentRecords.OrderBy(p => p.EmployeeId);
+   
+        public IEnumerable<SelectListItem> GetAllTaxYear()
+        {
+            var allTaxYear = _db.TaxYears.Select(taxYears => new SelectListItem
+            {
+                Text = taxYears.YearofTax,
+                Value = taxYears.Id.ToString()
+            });
+
+            return allTaxYear;
+        }
+
+        public PaymentRecord GetById(string id) => _db.PaymentRecords.Where(pay => pay.Id == id).FirstOrDefault();
+
+        public decimal NetPay(decimal totalEarnings, decimal totalDeduction) => totalEarnings - totalDeduction;
+
+        public decimal OvertimeEarnings(decimal overtimeRate, decimal overtimeHours) => overtimeHours * overtimeRate;
+
+        public decimal OvertimeHours(decimal hoursWorked, decimal contractualHours)
+        {
+            if (hoursWorked <= contractualHours)
+            {
+                overtimeHours = 0.00m;
+            }
+            else if (hoursWorked > contractualHours)
+            {
+                overtimeHours = hoursWorked - contractualHours;
+            }
+            return overtimeHours;
+        }
+
+        public decimal OvertimeRate(decimal hourlyRate) => hourlyRate * 1.5m;
+
+        public decimal TotalDeduction(decimal tax, decimal nic, decimal studentLoanRepayment, decimal unionFees) => tax + nic + studentLoanRepayment + unionFees;
+
+
+        public decimal TotalEarnings(decimal overtimeEarnings, decimal contractualEarnings)
+            => overtimeEarnings + contractualEarnings;
+    }
+}
---
+++ PayrollComputation/PayrollComputation.Services/Interfaces/IPayComputation.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using PayrollComputation.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollComputation.Services.Interfaces
+{
+    public interface IPayComputation
+    {
+        Task CreateAsync(PaymentRecord paymentRecord);
+        PaymentRecord GetById(string id);
+        IEnumerable<PaymentRecord> GetAll();
+        IEnumerable<SelectListItem> GetAllTaxYear();
+        decimal OvertimeHours(decimal hoursWorked, decimal contractualHours);
+        decimal ContractualEarnings(decimal contractualHours, decimal hoursWorked, decimal hourlyRate);
+        decimal OvertimeEarnings(decimal overtimeRate, decimal overtimeHours);
+        decimal TotalEarnings(decimal overtimeEarnings, decimal contractualEarnings);
+        decimal TotalDeduction(decimal tax, decimal nic, decimal studentLoanRepayment, decimal unionFees);
+        decimal NetPay(decimal totalEarnings, decimal totalDeduction);
+
+    }
+}
